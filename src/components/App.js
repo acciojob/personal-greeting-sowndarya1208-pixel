c755b8e5abc8732920b0d5b5d3370d6652ed6b80
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-02-04 22:58:55 +0530

    Implement name input with greeting functionality
    Added state management for name input and greeting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handleChange}
+      />
+
+      {name && <p>Hello, {name}!</p>}
+      
     </div>
   )
 }
 
-export default App
+export default App;
